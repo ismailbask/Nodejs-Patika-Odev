257c19b4356d80f363908c6a86b9cab6dca9c9f2
İsmail Basık <87307720+ismailbask@users.noreply.github.com>
2022-03-10 14:48:06 +0300

    Add files via upload

---
+++ alanHesapla.js
@@ -0,0 +1,7 @@
+const pi = 3.14;
+const arguments = process.argv.slice(2);
+function areaOfCircle(radius) {
+    return result = pi * radius * radius;
+    // console.log(result);
+}
+console.log(areaOfCircle(arguments[0]*1));
